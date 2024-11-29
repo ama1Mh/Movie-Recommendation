2a1eafebdb8b4d147e5f19bae748d896bdeee17a
Amal mohammed faqihi <97390633+ama1Mh@users.noreply.github.com>
2024-11-29 21:27:59 +0300

    import os

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify,  render_template
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+import os
 
 app = Flask(__name__)
 
