3666a68ebbb4832520c71bd4e744403a91b5a889
Amal mohammed faqihi <97390633+ama1Mh@users.noreply.github.com>
2024-11-29 21:23:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,7 @@ def recommend():
     # Call the recommendation logic
     recommendations = recommend_movies(selected_movies)
     return jsonify({"recommendations": recommendations})
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port)
